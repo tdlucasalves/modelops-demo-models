37a71900e96d40212055a086059dd0780cb35c7d
Christian Schmidt <christianaxel.schmidt@teradata.com>
2025-05-23 02:14:30 +0200

    VMO-2627: update STO to support py3.11 🏯

--- model_definitions/STO/model_modules/util.py
+++ model_definitions/STO/model_modules/util.py
@@ -7,7 +7,7 @@ def get_df_with_model(data_table: str,
 	query = f"""
 	SELECT d.*, CASE WHEN n_row=1 THEN m.model_artefact ELSE null END AS model 
 		FROM (SELECT x.*, ROW_NUMBER() OVER (PARTITION BY x.{partition_id} ORDER BY x.{partition_id}) AS n_row FROM {data_table} x) AS d
-		CROSS JOIN {model_artefacts_table} m
+		CROSS JOIN '{model_artefacts_table}' m
 		WHERE m.model_version = '{model_version}'
 	"""
 
